de27b84a336e620d79c895a5131507558e01b6e2
Reyhan Nur Tanrıtanır <134210477+reyhanmills@users.noreply.github.com>
2026-06-08 16:51:47 +0300

    feat: about section duzenlendi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,21 +2,22 @@ import circle from '../assets/circle.svg'
 
 function About() {
     return (
-        <section className='relative h-[544px] w-full bg-white py-16'>
-            <div className='mx-auto flex h-full max-w-[1440px] items-center'>
+        <section className='relative h-[544px] w-full overflow-hidden bg-white'>
+            <img
+                src={circle}
+                alt=""
+                className="absolute left-[-120px] top-[80px] top-1/2 w-[380px"
+            />
 
-                <div className='flex flex-1 items-center'>
-                    <img src={circle} alt="" className='w-[445px]' />
-                </div>
-
-                <div className="flex flex-1 flex-col">
-                    <h2 className='max-w-[527px] text-[40px] font-bold leading-[52px] text-[#000000]'>
+            <div className='relative z-10 mx-auto flex h-full max-w-[1200px] items-center justify-end'>
+                <div className="flex w-[527px] flex-col">
+                    <h2 className='text-[40px] font-bold leading-[52px] text-[#000000]'>
                         Find Match Now and
                         <br />
                         Develop Your Feelings
                     </h2>
 
-                    <p className='mt-6 max-w-[527px] text-[16px] leading-[32px] text-[#737373]'>
+                    <p className='mt-6 text-[16px] leading-[32px] text-[#737373]'>
                         There are so many platforms from this Pokan to make a task manager manage all your time, the data needed is very useful. and we have it all to get you to be successful
                     </p>
 
@@ -24,7 +25,6 @@ function About() {
                         Read More
                     </button>
                 </div>
-
             </div>
         </section>
     )
--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -16,36 +16,34 @@ function HowItWorks() {
                         Your Relationship
                     </h2>
                     <div className="mt-12 flex flex-col gap-8">
-                        <div className="mt-12 flex flex-col gap-8">
-                            <div className="flex items-center">
-                                <div className="flex h-[64px] w-[64px] shrink-0 items-center justify-center rounded-full bg-[#FF834F]/30">
-                                    <img src={Search} alt="" className="h-[24px] w-[24px]" />
-                                </div>
-
-                                <p className="ml-6 text-[16px] font-normal leading-[24px] text-[#737373]">
-                                    Find your dating relationship in app and waiting until you get a notification. have a good relationship its started
-                                </p>
+                        <div className="flex items-center">
+                            <div className="flex h-[64px] w-[64px] shrink-0 items-center justify-center rounded-full bg-[#FF834F]/30">
+                                <img src={Search} alt="" className="h-[24px] w-[24px]" />
                             </div>
 
-                            <div className="flex items-center">
-                                <div className="flex h-[64px] w-[64px] shrink-0 items-center justify-center rounded-full bg-[#FF834F]/30">
-                                    <img src={Heart} alt="" className="h-[24px] w-[24px]" />
-                                </div>
+                            <p className="ml-6 text-[16px] font-normal leading-[24px] text-[#737373]">
+                                Find your dating relationship in app and waiting until you get a notification. have a good relationship its started
+                            </p>
+                        </div>
 
-                                <p className="ml-6 text-[16px] font-normal leading-[24px] text-[#737373]">
-                                    Dating with benefits and you redefine the expectations of a perfect relationship with your partner
-                                </p>
+                        <div className="flex items-center">
+                            <div className="flex h-[64px] w-[64px] shrink-0 items-center justify-center rounded-full bg-[#FF834F]/30">
+                                <img src={Heart} alt="" className="h-[24px] w-[24px]" />
                             </div>
 
-                            <div className="flex items-center">
-                                <div className="flex h-[64px] w-[64px] shrink-0 items-center justify-center rounded-full bg-[#FF834F]/30">
-                                    <img src={Profile} alt="" className="h-[24px] w-[24px]" />
-                                </div>
+                            <p className="ml-6 text-[16px] font-normal leading-[24px] text-[#737373]">
+                                Dating with benefits and you redefine the expectations of a perfect relationship with your partner
+                            </p>
+                        </div>
 
-                                <p className="ml-6 text-[16px] font-normal leading-[24px] text-[#737373]">
-                                    Ideal relationship makes your online dating relationship run more smoothly using this app
-                                </p>
+                        <div className="flex items-center">
+                            <div className="flex h-[64px] w-[64px] shrink-0 items-center justify-center rounded-full bg-[#FF834F]/30">
+                                <img src={Profile} alt="" className="h-[24px] w-[24px]" />
                             </div>
+
+                            <p className="ml-6 text-[16px] font-normal leading-[24px] text-[#737373]">
+                                Ideal relationship makes your online dating relationship run more smoothly using this app
+                            </p>
                         </div>
                     </div>
                 </div>
